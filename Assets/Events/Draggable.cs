e907eba0c35f92a65532c0a0fa12a091f56171fa
Boud <maxime.boudreault.0@gmail.com>
2018-02-26 22:21:41 -0500

    Make event system demo more interesting

--- Assets/Events/Draggable.cs
+++ Assets/Events/Draggable.cs
@@ -1,21 +1,32 @@
 ﻿using UnityEngine;
 using UnityEngine.EventSystems;
 
+[RequireComponent(typeof(Rigidbody))]
 public class Draggable : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler
 {
     Vector3 delta;
+    Vector3 torque;
+    Rigidbody ownBody;
+
+    void Start()
+    {
+        ownBody = GetComponent<Rigidbody>();
+    }
 
     public void OnBeginDrag(PointerEventData data)
     {
         delta.Set(data.delta.x, data.delta.y, 0);
-        GetComponent<Rigidbody>().AddForce(delta * 100);
+        ownBody.AddForce(delta * 100);
     }
 
     public void OnDrag(PointerEventData eventData)
     {
+        torque.Set(eventData.delta.y, eventData.delta.x, 0);
+        ownBody.AddRelativeTorque(torque);
     }
 
     public void OnEndDrag(PointerEventData eventData)
     {
+        ownBody.position = Vector3.zero;
     }
 }
