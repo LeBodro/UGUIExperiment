7445441ba69965fa67bfd0e7d89e7011bb52304d
Boud <maxime.boudreault.0@gmail.com>
2019-02-20 10:21:13 -0500

    Add Event Trigger and UnityEvent examples to Ordering scene

---
+++ Assets/Ordering/Life.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(Image))]
+public class Life : MonoBehaviour
+{
+    [SerializeField] [Range(0, 1)] float dangerThreshold = 0.25f;
+    [SerializeField] Color danger = Color.red;
+    [SerializeField] Image fill;
+
+    [Header("Unity Event!")]
+    [SerializeField] UnityEvent onDeath;
+
+    bool isDead;
+
+    void Reset()
+    {
+        fill = GetComponent<Image>();
+    }
+
+    public void Hurt(float damage)
+    {
+        if (isDead)
+            return;
+
+        fill.fillAmount = Mathf.Max(0, fill.fillAmount - damage);
+        if (fill.fillAmount <= dangerThreshold)
+            fill.color = danger;
+
+        isDead = fill.fillAmount <= 0;
+
+        if (isDead)
+            onDeath.Invoke();
+    }
+}
